cba30a82aa3f2a562dce20f60bd2228f719c735e
Shelley Brook <sbrook13@gmail.com>
2020-06-27 00:33:35 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function shout(string){
+  return string.toUpperCase()
+}
+shout(hello)
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log("${string}").toUpperCase
+}
+logShout(hello)
