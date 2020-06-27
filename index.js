e7a619e3b4cec196674bedf4f35d63a0dda39f0b
Shelley Brook <sbrook13@gmail.com>
2020-06-27 00:52:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,11 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
-Function sayHiToGrandman(string){
-  }
+function sayHiToGrandma(string){
+  if(sayHiToGrandma===string.toLowerCase(){  
+    console.log("I can't hear you!")
+  } else if {(sayHiToGrandma===string.toUpperCase()console.log("YES INDEED!"));
+  else (sayHiToGrandma==="I love you, Grandma.")
+  return "I love you, too.")
+}
+  
